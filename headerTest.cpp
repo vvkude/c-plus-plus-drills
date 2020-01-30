137180e0d82b06e0aaef0584fb6f6c9ee0aa810e
Vik Kudesia <vvkudesia@gmail.com>
2020-01-29 22:19:06 -0500

    FIX gitignore UPDATE deprecated c++11 extensions

--- headerTest.cpp
+++ headerTest.cpp
@@ -1,4 +1,12 @@
-#include "headers/std_lib_facilities.h"
+/* includes from Stroustrup */
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<cmath>
+using namespace std;
+inline void keep_window_open() { char ch; cin>>ch; }
+
 int main()
 
 {
