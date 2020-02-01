e029bc8f4ed43817c8aa05fc32a90f3015e9dfc7
Vik Kudesia <vvkudesia@gmail.com>
2020-02-01 17:11:36 -0500

    PREPARE for standad error formatting exercises

--- facilitiesTests/typeTests.cpp
+++ facilitiesTests/typeTests.cpp
@@ -9,9 +9,8 @@ inline void keep_window_open() { char ch; cin>>ch; }
 int main()
 {
 	cout << "Please enter your first name and age\n";
-	string first_name;
-	int age;
-	cin >> first_name;
-	cin >> age;
+	string first_name ="???"; // initialize with some value so that erroneous output is at least standardized
+	int age=0; // initialize with some value so that erroneous output is at least standardized
+	cin >> first_name >> age;
 	cout << "Hello, " << first_name << " (age " << age << ")\n";
 }
