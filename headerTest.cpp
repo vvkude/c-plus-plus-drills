6f9122a80d6eebc6f84cf5cece0119dfc601b0a3
Vik Kudesia <vvkudesia@gmail.com>
2020-02-01 16:02:05 -0500

    ADD quick test of nonstandard includes for first part of Stroustrup book

--- headerTest.cpp
+++ headerTest.cpp
@@ -8,10 +8,11 @@ using namespace std;
 inline void keep_window_open() { char ch; cin>>ch; }
 
 int main()
-
 {
-    cout<<"Hello, World!\n"; 
-    keep_window_open(); // wait for a character to be entered
-
-    return 0;
+	cout << "Hi there! Please enter your first name (then hit 'enter'):\n";
+	string first_name; 
+	cin >> first_name;
+    cout << "Hello, " << first_name << "!\n"; 
+    // After testing this between Windows and Mac OS I am not sure if I need
+    // to add keep window open or not because it looks it's only buggy on Windows
 }
