a1f2b51b613ad58f82d2592b432b672a54d2022c
Vik Kudesia <vvkudesia@gmail.com>
2020-02-02 19:22:58 -0500

    UPDATE with simple error message function

--- labs/stroustrupDrills/chapter3.cpp
+++ labs/stroustrupDrills/chapter3.cpp
@@ -7,6 +7,13 @@ using namespace std;
 inline void keep_window_open() { char ch; cin>>ch; }
 
 // This is the drill from the end of chapter 3 in Stroustrup's Programming with C++
+void printErrorMessage(int num) {
+	if (num == 3) 
+	{cout << "AGE ERROR: That's impossibly young!" << endl; }
+    else if (num == 4)
+    {cout << "AGE ERROR: That's improbably old!" << endl; }
+}
+
 int main()
 {
 	cout << "Enter the name of the person who want to write to...\n";
@@ -20,11 +27,14 @@ int main()
 	     <<'\n';
 	char friend_sex;
 	cin >> friend_sex;
-	cout << "Enter your friend's age...\n";
 	string pronoun = "???";
 	    if (friend_sex == 'm') { pronoun = "he"; }
 	    else { pronoun = "she"; }
-	double age; // Don't need to restrict age to whole numbers
+    cout << "Enter your friend's age...\n";
+	double age = 0; // Let proud 12.5 year olds tell us about their exact age
+	cin >> age;
+	if (age <= 0) {printErrorMessage(3);}
+	if (age >= 100) {printErrorMessage(4);}
 	cout << "Dear, " 
 	     << first_name
 	     << "\n    How are you? I am fine. I miss you."
@@ -32,5 +42,14 @@ int main()
          << "\n    Have you seen " << friend_name << " around? I think "
          << pronoun << " wears a wig now?"
          << '\n';
+     if (age < 12)
+     	cout << "\n         Next year, you'll be " << age+1
+             << '\n';
+     else if (age == 17)
+     	cout << "\n         Next year, you'll be able to vote!"
+             << "\n";
+     else if (age > 70)
+     	cout << "\n         I hope you are enjoying retirement"
+             << '\n';
 }
 
