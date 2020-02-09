d7544a5c9e852440d9ce5507b2d23537641fa610
Vik Kudesia <vvkudesia@gmail.com>
2020-02-08 23:08:57 -0500

    FIX design to test for 0 in denominator FIX ability to check general input into program

--- labs/stroustrupDrills/exercise10.cpp
+++ labs/stroustrupDrills/exercise10.cpp
@@ -5,35 +5,35 @@
 /* Assignment:	Write a program that an operation followed by two operands
 				and output the result
 
+	Update: 	Didn't think to include the actual operators (+, -, *, /)
+
 	Approach:	Store the operation in a string and compare it like we did
 				in exercise 9 to determine the correct operation for the operands
 */
 
 int main()
 {
-	cout << "Enter an operation and two operands separated by spaces. I'll show you the result\n"
-	     << "Enter quit to stop this program\n";
 	string op = "???"; // initialize with some known value to aid in debugging
-	int operand01 = 0;
-	int operand02 = 0;
-    cin >> op >> operand01 >> operand02;
-
-    while (cin>>op) {
-        if (op == "add")
-    	    cout << operand01+operand02 << '\n';
-    	else if (op == "subtract")
-    		cout << operand01-operand02 << '\n';
-    	else if (op == "multiply")
-    		cout << operand01*operand02 << '\n';
-    	else if (op == "divide")
-    		cout << operand01/operand02 << '\n';
-    	else if (op == "quit") {
-    		cout << "Goodbye!\n";
-    	    return 0;
-    	}
-    	else 
-    		cout << "Not sure what you want me to do here. Can you try add, subtract, multiple, divide or quit?"
-    	         << "Enter an operation and two operands separated by spaces:\n";
-
-    }
+	double operand01 = 0;
+	double operand02 = 0;
+	cout << "Enter an operation and two operands separated by spaces. I'll show you the result\n"
+	     << "+, -, *, /, add, subtract, multiply and divide will work. Enter quit to stop this program\n";
+    
+    while(cin >> operand01 >> op >> operand02) { // easier way to perform steps if input is good
+        double result = 0;
+        if (op=="plus" || op=="+") result = operand01+operand02;
+        else if (op=="minus" || op=="-") result = operand01-operand02;
+        else if (op=="multiply" || op=="*") result = operand01*operand02;
+        else if (op=="divide" || op=="/") {
+        	// Coonditionally check that the denominator is not zero
+        	if (operand02==0) {
+        		cout << "We cannot divide by zero. Please enter another number:\n";
+        		cin >> operand02;
+        	}
+        	result = operand01/operand02;
+        } //--> /else-if
+        else 
+        	cout << "Sorry, I do not understand the operation you want";
+    cout << operand01 << " " << op << " " << operand02 << " == " << result << '\n';
+    }//--> /while
 }
