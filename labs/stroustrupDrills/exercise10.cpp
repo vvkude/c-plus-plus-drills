05602f51656dc56acacd890c77605ed6ef57a570
Vik Kudesia <vvkudesia@gmail.com>
2020-02-08 20:16:28 -0500

    ADD Exercise 10 accept an operator and two operands and show the result

---
+++ labs/stroustrupDrills/exercise10.cpp
@@ -0,0 +1,19 @@
+// Stroustrup's C++ Programming Chapter 3 Exercise 10
+// Accept an operator and two operands and output the result
+#include "../../headers/headerTemplate.h"
+
+/* Assignment:	Write a program that an operation followed by two operands
+				and output the result
+
+	Approach:	Store the operation in a string and compare it like we did
+				in exercise 9 to determine the correct operation for the operands
+*/
+
+int main()
+{
+	string op = "???"; // initialize with some known value to aid in debugging
+	cout << "Enter an operation and two operands separated by spaced. I'll show you the result";
+    cin >> op;
+
+
+}
