b19a5daae2aed8dcef5ccbe744d536ba6561cb79
Vik Kudesia <vvkudesia@gmail.com>
2020-02-09 14:48:07 -0500

    TIDY code and properly enumerate values of coins

--- labs/stroustrupDrills/coinCounter.cpp
+++ labs/stroustrupDrills/coinCounter.cpp
@@ -25,23 +25,40 @@ int main()
 	cout << "How many pennies do you have?\n";
 	int pennies = 0;
 	cin >> pennies;
-	// Program will add to cents every time the user tells us about a coin they have
-	// We could do this in one statement at the end, but it may help to debug to start this way
-	double cents = 0;
-	cents = pennies*.01;
 	cout << "How many nickels do you have?\n";
 	int nickels = 0;
 	cin >> nickels;
+	cout << "How many dimes do you have?\n";
+	int dimes = 0;
+	cin >> dimes;
+	cout << "How many quarters do you have?\n";
+	int quarters = 0;
+	cin >> quarters;
+	cout << "How many half-dollars do you have?\n";
+	int half_dollars = 0;
+	cin >> half_dollars;
+	cout << "How many dollar-coins do you have?\n";
+	int dollar_coins = 0;
+	cin >> dollar_coins;
+
+	// Calculate the total value in cents
+	// Note that this is an integer multiplied by a double
+	double cents = pennies*.01+nickels*.05+dimes*.1+quarters*.25+half_dollars*.5+dollar_coins*1;
 
     // Read back the denominations of each type of coin
     cout << "You have:\n"
          << pennies << " pennies\n"
+         << nickels << " nickels\n"
+         << dimes << " dimes\n"
+         << quarters << " quarters\n"
+         << half_dollars<< " half-dollars\n"
+         << dollar_coins << " dollar-coins\n"
          << '\n';
 
     // Avoid creating a new variable to store dollar amount by 
     // directly manipulating the cents variable
     cout << "The total value of your coins:\n"
          << "$" << cents << '\n'
-         << "Or, " << cents*100 << " cents";
+         << "Or, " << cents*100 << " cents\n";
 
 }
