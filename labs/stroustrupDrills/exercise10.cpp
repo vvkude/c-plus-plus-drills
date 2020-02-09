3ff2cb05444df76d3fe3778116ed685be9e251c4
Vik Kudesia <vvkudesia@gmail.com>
2020-02-08 20:37:55 -0500

    ADD operator and operand if clauses TO DO fix order inputs are accepted

--- labs/stroustrupDrills/exercise10.cpp
+++ labs/stroustrupDrills/exercise10.cpp
@@ -11,9 +11,29 @@
 
 int main()
 {
+	cout << "Enter an operation and two operands separated by spaces. I'll show you the result\n"
+	     << "Enter quit to stop this program\n";
 	string op = "???"; // initialize with some known value to aid in debugging
-	cout << "Enter an operation and two operands separated by spaced. I'll show you the result";
-    cin >> op;
+	int operand01 = 0;
+	int operand02 = 0;
+    cin >> op >> operand01 >> operand02;
 
+    while (cin>>op) {
+        if (op == "add")
+    	    cout << operand01+operand02 << '\n';
+    	else if (op == "subtract")
+    		cout << operand01-operand02 << '\n';
+    	else if (op == "multiply")
+    		cout << operand01*operand02 << '\n';
+    	else if (op == "divide")
+    		cout << operand01/operand02 << '\n';
+    	else if (op == "quit") {
+    		cout << "Goodbye!\n";
+    	    return 0;
+    	}
+    	else 
+    		cout << "Not sure what you want me to do here. Can you try add, subtract, multiple, divide or quit?"
+    	         << "Enter an operation and two operands separated by spaces:\n";
 
+    }
 }
